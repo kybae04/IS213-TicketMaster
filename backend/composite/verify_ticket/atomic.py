27788f47e97c14edce2ec585d21495b5c5ddf67e
rika <rikamiyachi.2023@scis.smu.edu.sg>
2025-03-26 10:19:16 +0800

    URLs for atomic

---
+++ backend/composite/verify_ticket/atomic.py
@@ -0,0 +1,3 @@
+TICKET_SERVICE_URL = "http://localhost:5005"
+EVENT_SERVICE_URL = "https://personal-d3kdunmg.outsystemscloud.com/ESDProject/rest/EventAPI/events"  
+SEAT_SERVICE_URL = "http://localhost:5000"
