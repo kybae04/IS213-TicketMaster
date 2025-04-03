51770d5de2898df3bd00ea2e8d242ad0887780d5
kybae04 <kyliebay@gmail.com>
2025-04-04 01:33:53 +0800

    removed unique=true for transactionID

--- backend/atomic/ticket/models.py
+++ backend/atomic/ticket/models.py
@@ -7,7 +7,7 @@ class Ticket(db.Model):
     seatID = db.Column(db.String(36), nullable=False)
     userID = db.Column(db.String(36), nullable=False)  # Current owner of ticket
     status = db.Column(db.String(20), nullable=False, default="pending_payment")
-    transactionID = db.Column(db.String(64), unique=True, nullable=True) 
+    transactionID = db.Column(db.String(64), nullable=True) 
     tradeRequestID = db.Column(db.String(64), nullable=True)  # Associated trade request ID
 
     def to_dict(self):
