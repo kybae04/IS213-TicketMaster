afa4cdf4e3256abefdb13a59590dc8558cb395fb
kybae04 <kyliebay@gmail.com>
2025-04-03 22:50:56 +0800

    remove idempotencyKey from db

--- backend/atomic/ticket/models.py
+++ backend/atomic/ticket/models.py
@@ -7,7 +7,6 @@ class Ticket(db.Model):
     eventID = db.Column(db.String(36), nullable=False)
     seatID = db.Column(db.String(36), nullable=False)
     userID = db.Column(db.String(36), nullable=False)  # Current owner of ticket
-    idempotencyKey = db.Column(db.String(64), unique=True, nullable=False)
     status = db.Column(db.String(20), nullable=False, default="pending_payment")
     transactionID = db.Column(db.String(64), unique=True, nullable=True) 
     tradeRequestID = db.Column(db.String(64), nullable=True)  # Associated trade request ID
