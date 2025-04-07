b3396fa1aee069cd05b2cbf2478b3153c2c18860
kybae04 <kyliebay@gmail.com>
2025-04-07 14:13:07 +0800

    remove tradeRequestID from ticket table

--- backend/atomic/ticket/models.py
+++ backend/atomic/ticket/models.py
@@ -8,8 +8,7 @@ class Ticket(db.Model):
     seatID = db.Column(db.String(36), nullable=False)
     userID = db.Column(db.String(36), nullable=False)  # Current owner of ticket
     status = db.Column(db.String(20), nullable=False, default="pending_payment")
-    transactionID = db.Column(db.String(64), nullable=True) 
-    tradeRequestID = db.Column(db.String(64), nullable=True)  # Associated trade request ID
+    transactionID = db.Column(db.String(64), nullable=True)
     listed_for_trade = db.Column(Boolean, nullable=False, default=False)
 
 
@@ -19,7 +18,6 @@ class Ticket(db.Model):
             "eventID": self.eventID,
             "seatID": self.seatID,
             "status": self.status,
-            "tradeRequestID": self.tradeRequestID,
             "transactionID": self.transactionID,
             "userID": self.userID,
             "listed_for_trade": self.listed_for_trade
